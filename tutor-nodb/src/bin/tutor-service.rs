c0793a883dc98270b7b327327c5cf429f12f46f6
heckfyxe <heckfyxe@gmail.com>
2021-09-16 08:50:32 +0300

    Added Course model. Added Course creation endpoint.

--- tutor-nodb/src/bin/tutor-service.rs
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -1,4 +1,4 @@
-use crate::routes::general_routes;
+use crate::routes::{course_routes, general_routes};
 use crate::state::AppState;
 use actix_web::{web, App, HttpServer};
 use std::io;
@@ -13,16 +13,21 @@ mod routes;
 #[path = "../handlers.rs"]
 mod handlers;
 
+#[path = "../models.rs"]
+mod models;
+
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     let shared_state = web::Data::new(AppState {
         health_check_response: "I'm good. You've already asked me ".to_string(),
         visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
     });
     let app = move || {
         App::new()
             .app_data(shared_state.clone())
             .configure(general_routes)
+            .configure(course_routes)
     };
 
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
