53f0a9724538807f34c1bd2788638e9974b6f10e
heckfyxe <heckfyxe@gmail.com>
2021-09-16 07:57:36 +0300

    Ready simple server with /health endpoint.

---
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -0,0 +1,29 @@
+use crate::routes::general_routes;
+use crate::state::AppState;
+use actix_web::{web, App, HttpServer};
+use std::io;
+use std::sync::Mutex;
+
+#[path = "../state.rs"]
+mod state;
+
+#[path = "../routes.rs"]
+mod routes;
+
+#[path = "../handlers.rs"]
+mod handlers;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    let shared_state = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+    });
+    let app = move || {
+        App::new()
+            .app_data(shared_state.clone())
+            .configure(general_routes)
+    };
+
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
